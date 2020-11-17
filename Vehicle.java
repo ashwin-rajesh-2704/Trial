082cd71aa21c8a6a65c02eee39431a0dd4e32b2e
ashwin-rajesh-2704 <73516686+ashwin-rajesh-2704@users.noreply.github.com>
2020-11-17 13:03:10 +0530

    Add files via upload
    Trial adding a repository

---
+++ Vehicle.java
@@ -0,0 +1,7 @@
+class Vehicle
+{
+ public static void main(String[] args)
+  { 
+    System.out.println("Which is your favourite car?");
+  }
+}
---
+++ my__db.java
@@ -0,0 +1,7 @@
+class Database
+{
+ 	public static void main(String[] args)
+	{
+		System.out.println("November 4, 2020");
+	}
+}
